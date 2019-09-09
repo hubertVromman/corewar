6e589d472ec40370869ce0a68c40c9b1dd68ce28
Hubert Vromman <hvromman@w-r9-p5.s19.be>
2019-09-09 19:47:14 +0200

    zjmp IDX_MOD

--- core/srcs/operations/zjmp.c
+++ core/srcs/operations/zjmp.c
@@ -17,7 +17,7 @@ int		operation_zjmp(t_champ *champ, t_proces *proces, t_arg *args)
 	champ = NULL;
 	if (proces->carry == 1 && args[0].type & T_DIR)
 	{
-		increment_pc(proces, args[0].value);
+		increment_pc(proces, args[0].value % IDX_MOD);
 	}
 	else
 	{
